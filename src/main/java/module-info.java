cd3e077035cf53f0fcabfd83dd3dad0043f02a61
Ethan McCue <ethan@mccue.dev>
2023-11-17 01:00:35 -0500

    remove unused library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ import org.jspecify.annotations.NullMarked;
 module dev.mccue.todoapp {
     requires org.microhttp;
     requires dev.mccue.microhttp.json;
-    requires dev.mccue.urlparameters;
     requires org.xerial.sqlitejdbc;
     requires org.slf4j.simple;
 
